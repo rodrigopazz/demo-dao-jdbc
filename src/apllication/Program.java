87fd56820f99024e412e08769cce540b0ec02d4a
rodrigopazz <rodrigopazz@gmail.com>
2021-11-26 14:59:53 -0400

    Department entity class

---
+++ src/apllication/Program.java
@@ -0,0 +1,14 @@
+package apllication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1, "Bokks");
+		System.out.println(obj);
+
+	}
+
+}
