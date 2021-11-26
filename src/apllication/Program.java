1fe9da9e3cf134fd44cfe0cd5a146b2591cf6573
rodrigopazz <rodrigopazz@gmail.com>
2021-11-26 15:32:26 -0400

    Seller entity class

--- src/apllication/Program.java
+++ src/apllication/Program.java
@@ -1,14 +1,22 @@
 package apllication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Bokks");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
 
+		
+		
 	}
 
 }
